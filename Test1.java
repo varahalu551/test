8cb035181492c44d4c12307b0f72d487886d0a23
varahalu51 <nagam.svb@gmail.com>
2023-09-25 12:15:41 +0530

    add Rama Mouli in array of name

--- Test1.java
+++ Test1.java
@@ -1,6 +1,6 @@
 public class Test1{
     public static void main(String a[]){
-        String name[]={"srinu","rama","vara"};
+        String name[]={"srinu","rama","vara","Rama Mouli"};
         for(String str: name)
         {
             System.out.println("Names are "+str);
