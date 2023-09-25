b5362a4913edc09747c0f68f903511e9b3206a28
varahalu51 <nagam.svb@gmail.com>
2023-09-25 12:12:59 +0530

    add file Test1

---
+++ Test1.java
@@ -0,0 +1,10 @@
+public class Test1{
+    public static void main(String a[]){
+        String name[]={"srinu","rama","vara"};
+        for(String str: name)
+        {
+            System.out.println("Names are "+str);
+        }
+
+    }
+}
